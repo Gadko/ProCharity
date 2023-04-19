abe662378fa39b010193c3d9e457681f71d98e10
Gadko <megotim@mail.ru>
2023-04-19 12:42:25 +0300

    ПОЧТИИ

--- src/index.js
+++ src/index.js
@@ -1,106 +1,120 @@
-import "./pages/index.css"
+import "./pages/index.css";
 
-import { formTextarea, whitelistValue, inputElm, inputMobile, buttonMobileClose, buttonMobileOpen, editAvatar } from "./utils/constants.js";
+import {
+  formTextarea,
+  whitelistValue,
+  inputElm,
+  inputMobile,
+  buttonMobileClose,
+  buttonMobileOpen,
+  editAvatar,
+} from "./utils/constants.js";
 
 import Textarea from "./components/Textarea.js";
-import Cropper from "../node_modules/cropperjs/dist/cropper.js"
-import ItcCustomSelect from "./components/itc-custom-select.js"
+import Cropper from "../node_modules/cropperjs/dist/cropper.js";
+import ItcCustomSelect from "./components/itc-custom-select.js";
 
-const textarea = new Textarea(formTextarea, '.input-text__counter')
-textarea.enableTextarea()
+const textarea = new Textarea(formTextarea, ".input-text__counter");
+textarea.enableTextarea();
 
-import img from './images/chevron-down.svg'
+import img from "./images/chevron-down.svg";
 
-import { tagTemplate, suggestionItemTemplate } from './utils/tagify_templates.js';
-
-buttonMobileOpen.addEventListener('click', () => {
-  inputMobile.classList.add('competitions-mobile_opened')
+import {
+  tagTemplate,
+  suggestionItemTemplate,
+} from "./utils/tagify_templates.js";
+if (window.location.pathname === "/competitions.html") {
+buttonMobileOpen.addEventListener("click", () => {
+  inputMobile.classList.add("competitions-mobile_opened");
 });
-buttonMobileClose.addEventListener('click', () => {
-  inputMobile.classList.remove('competitions-mobile_opened')
-})
+buttonMobileClose.addEventListener("click", () => {
+  inputMobile.classList.remove("competitions-mobile_opened");
+});
 
 let tagify = new Tagify(inputElm, {
-    userInput : false,
-    dropdown: {
-        closeOnSelect: false,
-        maxItems: Infinity,
-        classname: 'groupList',
-    },
-    templates: {
-        tag: tagTemplate,
-        dropdownItem: suggestionItemTemplate,
-    },
-    whitelist: whitelistValue,
-})
+  userInput: false,
+  dropdown: {
+    closeOnSelect: false,
+    maxItems: Infinity,
+    classname: "groupList",
+  },
+  templates: {
+    tag: tagTemplate,
+    dropdownItem: suggestionItemTemplate,
+  },
+  whitelist: whitelistValue,
+});
 
-tagify.dropdown.createListHTML = sugegstionsList  => {
-    
-    const getCompetitiesForGroup = (group) => {
-      let competities = []
-      sugegstionsList.forEach(sug => {
-        if(sug.group === group) {
-          competities.push(tagify.settings.templates.dropdownItem.apply(tagify, [sug]));
-        }
-      });
-      return competities.join("");
-    }
-
-    let items = []
-
-    let groups = []
-    sugegstionsList.forEach(sug => {
-      groups.push(sug.group)
+tagify.dropdown.createListHTML = (sugegstionsList) => {
+  const getCompetitiesForGroup = (group) => {
+    let competities = [];
+    sugegstionsList.forEach((sug) => {
+      if (sug.group === group) {
+        competities.push(
+          tagify.settings.templates.dropdownItem.apply(tagify, [sug])
+        );
+      }
     });
-    groups = new Set(groups); // Убираем дубликаты
+    return competities.join("");
+  };
 
-    groups.forEach(group => {
-       items.push(`<div class="tagify__dropdown__itemsGroup" data-title="Group ${group}:">
+  let items = [];
+
+  let groups = [];
+  sugegstionsList.forEach((sug) => {
+    groups.push(sug.group);
+  });
+  groups = new Set(groups); // Убираем дубликаты
+
+  groups.forEach((group) => {
+    items.push(`<div class="tagify__dropdown__itemsGroup" data-title="Group ${group}:">
                           <div style="display: flex; justify-content: space-between">
-                            <span class="group__span">${ group }</span>
+                            <span class="group__span">${group}</span>
                             <img src=${img} style="padding: 20px;">
                         
                             <div class="items">
-                              <div class="tagify_subgroup group_${ group }">
-                                  ${ getCompetitiesForGroup(group) }
+                              <div class="tagify_subgroup group_${group}">
+                                  ${getCompetitiesForGroup(group)}
                               </div>
                             </div>
                           </div>
                     </div>`);
-    });
+  });
 
-    let resultHtml = `<div class="multi-level">
+  let resultHtml = `<div class="multi-level">
                         <div class="items">
                           ${items.join("")}
                         </div>
                       </div
-                     `
+                     `;
 
-    return resultHtml;
-}
+  return resultHtml;
+};
 
 function generateMobileSeletion() {
   let list = document.querySelector(".competitions-mobile__list");
 
   const getCompetitiesForGroup = (group) => {
-      let competities = []
-      whitelistValue.forEach(sug => {
-        if(sug.group === group) {
-          competities.push(`<p class="competitions-mobile__chip">${sug.value}</p>`);
-        }
-      });
-      return competities.join("");
-    }
-
-    let items = []
-
-    let groups = []
-    whitelistValue.forEach(sug => {
-      groups.push(sug.group)
+    let competities = [];
+    whitelistValue.forEach((sug) => {
+      if (sug.group === group) {
+        competities.push(
+          `<p class="competitions-mobile__chip">${sug.value}</p>`
+        );
+      }
     });
-    groups = new Set(groups); // Убираем дубликаты
-    groups.forEach(group => {
-       items.push(`<li class="competitions-mobile__container">
+    return competities.join("");
+  };
+
+  let items = [];
+
+  let groups = [];
+  whitelistValue.forEach((sug) => {
+    groups.push(sug.group);
+  });
+  groups = new Set(groups); // Убираем дубликаты
+  groups.forEach((group) => {
+    items.push(`<li class="competitions-mobile__container">
               <label class="competitions-mobile__acoprd-content">
                 <input
                   class="competitions-mobile__input"
@@ -118,28 +132,30 @@ function generateMobileSeletion() {
                       Выбрать все
                     </button>
                   </div>
-                  ${ getCompetitiesForGroup(group)}
+                  ${getCompetitiesForGroup(group)}
                 </div>
               </label>
             </li>`);
-    });
+  });
 
-    let resultHtml = items.join(""); 
-                     
-  list.insertAdjacentHTML('beforeend', resultHtml);
+  let resultHtml = items.join("");
+
+  list.insertAdjacentHTML("beforeend", resultHtml);
 }
 
 generateMobileSeletion();
+}
 
-const select1 = new ItcCustomSelect('#select-1');
+if (window.location.pathname === "/contacts.html") {
+const select1 = new ItcCustomSelect("#select-1");
 
-const select2 = new ItcCustomSelect('#select-2');
-    
+const select2 = new ItcCustomSelect("#select-2");
 
-editAvatar.src = "https://wp-s.ru/wallpapers/6/2/288535681420729/velikolepnyj-leopard-smotrit-vdal.jpg"
+editAvatar.src =
+  "https://wp-s.ru/wallpapers/6/2/288535681420729/velikolepnyj-leopard-smotrit-vdal.jpg";
 const cropper = new Cropper(editAvatar, {
-    aspectRatio: 1/1,
-    autoCropArea: 1
+  aspectRatio: 1 / 1,
+  autoCropArea: 1,
 });
 
 cropper.options.highlight = false;
@@ -148,4 +164,4 @@ cropper.options.guides = false;
 cropper.options.center = false;
 cropper.options.cropBoxMovable = false;
 cropper.options.background = false;
-
+}
